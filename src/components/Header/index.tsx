4f2f957f596153e52a7d77328f4f5cd3c1746b7a
Luan Leone <luanleone05@gmail.com>
2021-03-31 21:01:57 -0300

    :rocket: first commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,16 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link';
+
+import styles from './header.module.scss';
+
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export function Header() {
+  return (
+    <header className={styles.container}>
+      <Link href="/">
+        <a>
+          <img src="spacetraveling.svg" alt="logo" />
+        </a>
+      </Link>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import Head from 'next/head';
+import { Header } from '../components/Header';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
@@ -24,9 +28,32 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Space Traveling</title>
+      </Head>
+      <div className={styles.container}>
+        <Header />
+        <main className={commonStyles.content}>
+          <section>
+            <a>
+              <h2>lorem ipsum dolor lorem ips</h2>
+              <p>lorem ipsum dolor sit amet lorem</p>
+              <span>
+                <FiCalendar color="#BBBBBB" fontSize="20px" /> 31 Mar 2021
+                <FiUser color="#BBBBBB" fontSize="20px" /> Luan Leone
+              </span>
+            </a>
+          </section>
+        </main>
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
